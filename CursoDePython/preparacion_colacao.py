0add60f944737fbfc4b7d709f7479c3023aca51f
Hugoking <hugo@mscreativos.com>
2025-02-12 13:50:47 +0100

    12/02 13:50

--- CursoDePython/preparacion_colacao.py
+++ CursoDePython/preparacion_colacao.py
@@ -3,6 +3,19 @@ print("Me voy a la nevera")
 print("Abro la nevera")
 
 hay_leche = input("¿Hay leche? (S/N) ")
+hay_colacao = input("¿Hay colacao? (S/N) ")
 
-if hay_leche == "S":
-    print("Sacamos la leche de la nevera")
+if hay_leche != "S" or hay_colacao != "S":
+    print("Voy al super")
+    if hay_leche != "S":
+        print("Compro leche")
+    if hay_colacao != "S":
+        print("Compro colacao")
+    
+    print("Ponemos la leche en el vaso")
+    print("Añadimos el colacao en el vaso")
+    print("Mezclamos")
+        
+        
+        
+# print("Sacamos la leche de la nevera")
